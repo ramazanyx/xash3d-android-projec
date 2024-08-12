c43173e8cf91979fd79eafd7a2f374228cd9ef92
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-08-12 22:07:51 +0300

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -46,7 +46,7 @@ XASH3D_CONFIG := $(APPLICATIONMK_PATH)/xash3d_config.mk
 
 ifeq ($(XASH_64BIT),1)
 #APP_ABI := x86_64 arm64-v8a
-APP_ABI := arm64-v8a
+APP_ABI := arm64-v8a armeabi-v7a-hard
 else
 APP_ABI := x86 armeabi-v7a-hard
 endif
